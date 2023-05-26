4ff789a02af40d33e5e125dcd34ceaaff78c3a9f
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-05-26 16:17:14 -0400

    requets update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,23 @@
 import asyncHandler from "express-async-handler";
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error("Something went wrong");
-
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+const getUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get User" });
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User" });
+});
+
+export { authUser, registerUser, logoutUser, getUser, updateUser };
